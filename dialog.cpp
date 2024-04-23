99f0e615e46c11dbde5d9d87669138ddb563c980
Viktor <11_70@bk.ru>
2024-04-23 22:21:39 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -45,13 +45,55 @@ Dialog::Dialog(QWidget *parent)
           ui->setupUi(this);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- listDirectory("/mnt/97d838cf-b3a1-4c1d-9c9d-cf745cf3116a/загрузки/imageye2/", 
- "/home/viktor/my_projects_qt_2/paketnoe_masshtabirovanie_kartinok/spisok.txt");
-///           
+/// convert путь-к-картинке -resize 10x10 -background White -gravity center -extent 10x10 путь-к-выходной-картинке
+/// /mnt/97d838cf-b3a1-4c1d-9c9d-cf745cf3116a/загрузки/imageye2/vihod/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //   QString myString;
+    const int len = 2832, strings =357;
+    const char ch = '\n';
+    char mass[len][strings];
+    
+    ifstream fs("/home/viktor/my_projects_qt_2/paketnoe_masshtabirovanie_kartinok/spisok.txt", ios::in | ios::binary);
+    
+    QString comanda1="convert \"";    
+      QString puty;
+    QString comanda3="\" -resize 10x10 -background White -gravity center -extent 10x10 \"";
+    QString puty_vihod="/mnt/97d838cf-b3a1-4c1d-9c9d-cf745cf3116a/загрузки/imageye2/vihod/";
+   QString comanda4=".bmp\"";
+    QString comanda_vsia;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    for(int r = 0; r<strings; r++)
+    {
+        fs.getline(mass[r], len-1,ch); //Считываем строки в массив
+        
+        puty=mass[r];
+        // subString = myString.mid(7,myString.size() - 1);
+        comanda_vsia=comanda1+puty+comanda3+puty_vihod+QString::number(r)+comanda4;
+        system (comanda_vsia.toStdString().c_str() );
+     
+    }
+
+    fs.close(); //Закрываем файл 
+/// 
+/// 
+///
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// / 
+         
      }
 
 Dialog::~Dialog()
      {
           delete ui;
      }
+
+
+void Dialog::on_pushButton_clicked()
+{
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ listDirectory("/mnt/97d838cf-b3a1-4c1d-9c9d-cf745cf3116a/загрузки/imageye2/", 
+ "/home/viktor/my_projects_qt_2/paketnoe_masshtabirovanie_kartinok/spisok.txt");
+///  
+}
 
--- dialog.h
+++ dialog.h
@@ -15,6 +15,9 @@ class Dialog : public QDialog
           Dialog(QWidget *parent = nullptr);
           ~Dialog();
           
+     private slots:
+          void on_pushButton_clicked();
+          
      private:
           Ui::Dialog *ui;
      };
